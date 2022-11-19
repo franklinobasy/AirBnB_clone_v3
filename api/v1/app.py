85740b855003c676b8c9e9415872ecaff4260334
Franklin Obasi <franklinobasy@gmail.com>
2022-11-19 12:04:12 +0100

    completed task - 5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 ''' app.py '''
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 
 
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_app(exception):
     """Teardown the app"""
     storage.close()
+
+
+@app.errorhandler(404)
+def route_not_found(e):
+    """returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"})
 
 
 if __name__ == "__main__":
