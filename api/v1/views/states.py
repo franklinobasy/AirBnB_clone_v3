a9c6985c9fb3554b38e9ab5a6e3df3863471e3bc
Franklin Obasi <franklinobasy@gmail.com>
2022-11-19 14:30:51 +0100

    Added delete route to states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -17,9 +17,20 @@ def get_states():
 
 
 @app_views.route("/states/<id>", methods=["GET"], strict_slashes=False)
-def get_states_id(id):
+def get_state_id(id):
     '''Retrieves a State object'''
     state_obj = storage.get("State", id)
     if state_obj:
         return jsonify(state_obj.to_dict())
     return abort(404)
+
+
+@app_views.route("/states/<id>", methods=["DELETE"], strict_slashes=False)
+def delete_state_id(id):
+    '''Deletes a State object'''
+    state_obj = storage.get("State", id)
+    if state_obj:
+        storage.delete(state_obj)
+        return jsonify({}), 200
+    
+    return abort(400)
