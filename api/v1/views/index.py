d24e54fd2b14ccff34ba9bce63bf89ef2a752712
Franklin Obasi <franklinobasy@gmail.com>
2022-11-19 10:02:00 +0100

    Started working on task - 4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,4 +7,11 @@ from api.v1.views import app_views
 
 @app_views.route("/status")
 def index():
+    """Endpoint to return status of the api"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """An endpoint that retrieves the number of each objects by type"""
+    pass
