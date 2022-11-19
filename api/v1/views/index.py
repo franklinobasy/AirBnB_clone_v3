ff447c8a35d03f827105a0b3c1ee4b2f3314740a
Franklin Obasi <franklinobasy@gmail.com>
2022-11-19 11:40:17 +0100

    completed working on task - 4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,16 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
+classes = {
+    "amenities" : "Amenity",
+    "cities" : "City",
+    "places" : "Place",
+    "reviews" : "Review",
+    "states" : "State",
+    "users" : "User"
+}
 
 @app_views.route("/status")
 def index():
@@ -14,4 +23,8 @@ def index():
 @app_views.route("/stats")
 def stats():
     """An endpoint that retrieves the number of each objects by type"""
-    pass
+    stats = {}
+    for name, cls in classes.items():
+        stats[name] = storage.count(cls)
+
+    return jsonify(stats)
