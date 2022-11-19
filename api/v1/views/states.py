df1edb39e8dfbe79e04bcab95d4e9e2d3ad4acd6
Franklin Obasi <franklinobasy@gmail.com>
2022-11-19 17:42:45 +0100

    complete task - 6. State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 ''' states.py'''
 
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
+from models.state import State
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
@@ -22,7 +23,7 @@ def get_state_id(id):
     state_obj = storage.get("State", id)
     if state_obj:
         return jsonify(state_obj.to_dict())
-    return abort(404)
+    abort(404)
 
 
 @app_views.route("/states/<id>", methods=["DELETE"], strict_slashes=False)
@@ -31,6 +32,45 @@ def delete_state_id(id):
     state_obj = storage.get("State", id)
     if state_obj:
         storage.delete(state_obj)
+        storage.save()
         return jsonify({}), 200
-    
-    return abort(400)
+
+    abort(400)
+
+
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
+def create_state():
+    '''Creates a State'''
+    if request.headers.get('Content-Type') != "application/json":
+        abort(400, description="Not a JSON")
+
+    state_json = request.get_json()
+
+    if not state_json.get("name"):
+        abort(400, description="Missing name")
+
+    state_obj = State(**state_json)
+    storage.new(state_obj)
+    storage.save()
+
+    return jsonify(state_obj.to_dict()), 200
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
+def update_state(state_id):
+    '''Updates a State object'''
+    state_obj = storage.get("State", state_id)
+    if not state_obj:
+        abort(404)
+
+    if request.headers.get('Content-Type') != "application/json":
+        abort(400, description="Not a JSON")
+
+    state_json = request.get_json()
+    for attr, attr_value in state_json.items():
+        if attr not in ["id", "created_at", "updated_at"]:
+            setattr(state_obj, attr, attr_value)
+
+    storage.save()
+
+    return jsonify(state_obj.to_dict()), 200
